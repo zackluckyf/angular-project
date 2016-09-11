1467460ca27e5352896c5a4b022f487d473586f5
zackluckyf <fanning.34@gmail.com>
2016-09-10 21:34:11 -0400

    setup some server stuff need to probably just go back and work on wiring

--- app/pages/yourTeam/yourTeamController.js
+++ app/pages/yourTeam/yourTeamController.js
@@ -10,5 +10,20 @@
             // this clears the input fields after a player is added!
             this.player1 = '';
         };
+        // this function is to attempt to talk to the server
+        this.button = function() {
+            function returnFeatures(response) {
+                return response.map(function(item) {
+                    if (item === 'Carson Palmer') {
+                        console.log('Carson Palmer');
+                    }
+                    return item === 'Carson Palmer';
+                });
+            }
+            (function loadPhones() {
+                return $http.get('http://localhost:8080/yourTeam')
+                    .then(returnFeatures);
+            })();
+        };
     }
 }());
