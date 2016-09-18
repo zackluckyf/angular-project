cf2217fd792274253bf396913b50363ad4ad1214
zackluckyf <fanning.34@gmail.com>
2016-09-17 20:36:42 -0400

    protractor has one test working now

--- tests/e2e/homepage.js
+++ tests/e2e/homepage.js
@@ -1,47 +1,51 @@
 /*jshint esversion: 6 */
-var homepage = (function () {
-  'use strict';
-  browser.get('index.html');
-  /*
-  TALK TO DANE ABOUT BETTER WAY TO SELECT ELEMENTS, this relies on checking
-  for them and then it automatically selects the first since there are multiple
-  copies of the same elements
-  */
-  var remove = element(by.css('.remove'));
-  var player = element(by.binding('player.name'));
-  var playerInput = element(by.css('.player-inputs'));
-  var add = element(by.css('.add'));
-  var next = element(by.id('next-button'));
-  var analyze = element(by.css('.analyze'));
-  var tradeValue = element(by.binding('dc.advice[2]'));
-  var getTitleText = function () {
-    return title.getText();
-  };
-  var clickRemove = function () {
-    remove.click();
-  };
-  var getPlayerText = function () {
-    return player.getText();
-  };
-  var addPlayer = function (string) {
-    playerInput.sendKeys(string);
-    add.click();
-  };
-  var navigate = function () {
-    next.click();
-    analyze.click();
-  };
-  var tradeValueText = function () {
-    return tradeValue.getText();
-  };
+var homepage = (function() {
+    'use strict';
+    browser.get('index.html');
+    /*
+    TALK TO DANE ABOUT BETTER WAY TO SELECT ELEMENTS, this relies on checking
+    for them and then it automatically selects the first since there are multiple
+    copies of the same elements
+    */
+    var remove = element(by.css('.remove'));
+    var teamTitle = element(by.css('.sub-title'));
+    var player = element(by.binding('player.name'));
+    var playerInput = element(by.css('.player-inputs'));
+    var add = element(by.css('.add'));
+    var next = element(by.id('next-button'));
+    var analyze = element(by.css('.analyze'));
+    var tradeValue = element(by.binding('dc.advice[2]'));
+    var getTitleText = function() {
+        return title.getText();
+    };
+    var clickRemove = function() {
+        remove.click();
+    };
+    var getPlayerText = function() {
+        return player.getText();
+    };
+    var addPlayer = function(string) {
+        playerInput.sendKeys(string);
+        add.click();
+    };
+    var navigate = function() {
+        next.click();
+        analyze.click();
+    };
+    var tradeValueText = function() {
+        return tradeValue.getText();
+    };
+    var titleCheck = function() {
+        return teamTitle.getText();
+    };
 
-  return {
-    getPlayerText: getPlayerText,
-    clickRemove: clickRemove,
-    addPlayer: addPlayer,
-    navigate: navigate,
-    tradeValueText: tradeValueText
-  };
+    return {
+        getPlayerText: getPlayerText,
+        clickRemove: clickRemove,
+        addPlayer: addPlayer,
+        navigate: navigate,
+        tradeValueText: tradeValueText
+    };
 }());
 
 module.exports = homepage;
--- tests/e2e/myApp.Protractor.spec.js
+++ tests/e2e/myApp.Protractor.spec.js
@@ -1,22 +1,25 @@
 // protractor
 var homepage = require('./homepage');
-xdescribe('myApp', function() {
+describe('myApp', function() {
     'use strict';
-    it('should have a player text of Adrian Peterson', function() {
+    it("should have a your team title on the first page ", function() {
+        expect(homepage.titleCheck()).toEqual('Your Team');
+    });
+    xit('should have a player text of Adrian Peterson', function() {
         expect(homepage.getPlayerText()).toEqual('Adrian Peterson');
     });
-    it('should remove a player once remove is clicked', function() {
+    xit('should remove a player once remove is clicked', function() {
         homepage.clickRemove();
         // removes Adrian Peterson leaving Carson Palmer
         expect(homepage.getPlayerText()).toEqual('Carson Palmer');
     });
-    it("should add a player once a player's name is entered", function() {
+    xit("should add a player once a player's name is entered", function() {
         homepage.clickRemove();
         // removes Carson Palmer Leaving no one
         homepage.addPlayer('Antonio Brown');
         expect(homepage.getPlayerText()).toEqual('Antonio Brown');
     });
-    it('should navigate through pages and get the results of a trade', function() {
+    xit('should navigate through pages and get the results of a trade', function() {
         // two teams should currently be Antonio Brown and Antonio Brown
         // without the state of the controllers transferring it doesn't keep state between views/pages
         homepage.navigate();
