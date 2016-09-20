5a96715328329b643bb20fb05623bdf7ed5553e7
zackluckyf <fanning.34@gmail.com>
2016-09-19 20:46:51 -0400

    fixed protractor

--- tests/e2e/homepage.js
+++ tests/e2e/homepage.js
@@ -2,37 +2,17 @@
 var homepage = (function() {
     'use strict';
     browser.get('index.html');
-    /*
-    TALK TO DANE ABOUT BETTER WAY TO SELECT ELEMENTS, this relies on checking
-    for them and then it automatically selects the first since there are multiple
-    copies of the same elements
-    */
-    var remove = element(by.css('.remove'));
+
     var teamTitle = element(by.css('.sub-title'));
-    var player = element(by.binding('player.name'));
-    var playerInput = element(by.css('.player-inputs'));
-    var add = element(by.css('.add'));
     var next = element(by.id('next-button'));
-    var analyze = element(by.css('.analyze'));
-    var tradeValue = element(by.binding('dc.advice[2]'));
-    var getTitleText = function() {
-        return title.getText();
-    };
-    var clickRemove = function() {
-        remove.click();
-    };
-    var getPlayerText = function() {
-        return player.getText();
-    };
-    var addPlayer = function(string) {
-        playerInput.sendKeys(string);
-        add.click();
-    };
+    var analyze = element(by.id('analyze'));
+    var newTrade = element(by.id('new-trade'));
+    var tradeValue = element(by.css('results'));
+
     var navigate = function() {
         next.click();
         analyze.click();
-    };
-    var tradeValueText = function() {
+        newTrade.click();
         return tradeValue.getText();
     };
     var titleCheck = function() {
@@ -40,11 +20,8 @@ var homepage = (function() {
     };
 
     return {
-        getPlayerText: getPlayerText,
-        clickRemove: clickRemove,
-        addPlayer: addPlayer,
         navigate: navigate,
-        tradeValueText: tradeValueText
+        titleCheck: titleCheck
     };
 }());
 
--- tests/e2e/myApp.Protractor.spec.js
+++ tests/e2e/myApp.Protractor.spec.js
@@ -5,24 +5,8 @@ describe('myApp', function() {
     it("should have a your team title on the first page ", function() {
         expect(homepage.titleCheck()).toEqual('Your Team');
     });
-    xit('should have a player text of Adrian Peterson', function() {
-        expect(homepage.getPlayerText()).toEqual('Adrian Peterson');
-    });
-    xit('should remove a player once remove is clicked', function() {
-        homepage.clickRemove();
-        // removes Adrian Peterson leaving Carson Palmer
-        expect(homepage.getPlayerText()).toEqual('Carson Palmer');
-    });
-    xit("should add a player once a player's name is entered", function() {
-        homepage.clickRemove();
-        // removes Carson Palmer Leaving no one
-        homepage.addPlayer('Antonio Brown');
-        expect(homepage.getPlayerText()).toEqual('Antonio Brown');
-    });
+    console.log('temporarily disabled until navigation can be brought back');
     xit('should navigate through pages and get the results of a trade', function() {
-        // two teams should currently be Antonio Brown and Antonio Brown
-        // without the state of the controllers transferring it doesn't keep state between views/pages
-        homepage.navigate();
-        expect(homepage.tradeValueText()).toEqual('This is a balanced trade!');
+        expect(homepage.navigate()).toEqual('This is a balanced trade!');
     });
 });
