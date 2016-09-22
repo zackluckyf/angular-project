47df7d98dfa8485b5d1b1662696b0a5aedcaeb75
zackluckyf <fanning.34@gmail.com>
2016-09-22 10:36:51 -0400

    fixed iife

--- src/app/app.js
+++ src/app/app.js
@@ -1,46 +1,46 @@
-(function() {
-    'use strict';
-    require('./common/team/teamConfig');
-    require('./yourTeam/yourTeamConfig');
-    require('./otherTeam/otherTeamConfig');
-    require('./analyzer/analyzerConfig');
+(function () {
+  'use strict';
+  require('./common/team/teamConfig');
+  require('./yourTeam/yourTeamConfig');
+  require('./otherTeam/otherTeamConfig');
+  require('./analyzer/analyzerConfig');
 
-    reroute.$inject = ['$urlRouterProvider'];
+  reroute.$inject = ['$urlRouterProvider'];
 
-    function reroute($urlRouterProvider) {
-        $urlRouterProvider.otherwise('/yourTeam');
+  function reroute ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/yourTeam');
+  }
+
+  function SetupFactory () {
+    var vm = this;
+    this.yourTeam = {
+      name: 'yourTeam',
+      players: []
+    };
+    this.otherTeam = {
+      name: 'otherTeam',
+      players: []
+    };
+
+    function teamState (name) {
+      if (name === 'yourTeam') {
+        return vm.yourTeam;
+      } else return vm.otherTeam;
     }
 
-    function SetupFactory() {
-        var vm = this;
-        this.yourTeam = {
-            name: 'yourTeam',
-            players: []
-        };
-        this.otherTeam = {
-            name: 'otherTeam',
-            players: []
-        };
-
-        function teamState(name) {
-            if (name === 'yourTeam') {
-                return vm.yourTeam;
-            } else return vm.otherTeam;
-        }
-
-        function setTeam(team, player) {
-            if (team.name === 'yourTeam') {
-                vm.yourTeam.players.push(player);
-            } else vm.otherTeam.players.push(player);
-        }
-
-        return {
-            teamState: teamState,
-            setTeam: setTeam
-        };
+    function setTeam (team, player) {
+      if (team.name === 'yourTeam') {
+        vm.yourTeam.players.push(player);
+      } else vm.otherTeam.players.push(player);
     }
 
-    angular.module('myApp', ['myApp.team', 'myApp.yourTeam', 'myApp.otherTeam', 'myApp.analyzer'])
-        .config(reroute)
-        .factory('SetupFactory', SetupFactory);
-})();
+    return {
+      teamState: teamState,
+      setTeam: setTeam
+    };
+  }
+
+  angular.module('myApp', ['myApp.team', 'myApp.yourTeam', 'myApp.otherTeam', 'myApp.analyzer'])
+    .config(reroute)
+    .factory('SetupFactory', SetupFactory);
+}());
